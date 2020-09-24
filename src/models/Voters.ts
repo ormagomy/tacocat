4ccb126506e40b4bf6abff11450f894cfde5ab8a
jorme <jared_orme@intuit.com>
2020-09-24 11:18:38 -0600

    Added Voter model

---
+++ src/models/Voters.ts
@@ -0,0 +1,10 @@
+export interface Voter {
+    id: number;
+    firstname: string;
+    lastname: string;
+    address: string;
+    city: string;
+    birthdate: string;
+    email: string;
+    phone: string;
+}
