e7f88fc72968704b43130056da52979b5fd46de0
Sunil Singamsetty <sunil_singamsetty@intuit.com>
2020-09-24 11:28:02 -0700

    voter registration initial commit

--- src/components/VoterForm.tsx
+++ src/components/VoterForm.tsx
@@ -13,8 +13,36 @@ let testDate = {
 
 export function VoterForm() {
     return (
+        <form>
         <div>
             <h2>Voter Form</h2>
         </div>
+        <div>
+        <label>
+            ID <input type="integer" name="id" value={testDate.id}/>
+        </label>
+        <label>
+            First Name <input type="text" name="firstname" value={testDate.firstname}/>
+        </label>
+        <label>
+            Last Name <input type="text" name="lastname" value={testDate.lastname}/>
+        </label>
+        <label>
+            Address <input type="text" name="address" value={testDate.address}/>
+        </label>
+        <label>
+            City <input type="text" name="city" value={testDate.city}/>
+        </label>
+        <label>
+            Birth Date <input type="text" name="birthdate" value={testDate.birthdate}/>
+        </label>
+        <label>
+            Email Address <input type="text" name="email" value={testDate.email}/>
+        </label>
+        <label>
+            Phone Number <input type="text" name="phone" value={testDate.phone}/>
+        </label>
+        </div>
+        </form>
     );
 }
