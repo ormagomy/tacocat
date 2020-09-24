8fb8c1b3e047498dcbf15132dcf634eca98772ad
jdroigk <john_droigk@intuit.com>
2020-09-24 10:06:47 -0700

    add form

---
+++ src/components/VoterForm.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+let testDate = {
+    "id": 123,
+    "firstname": "Fred",
+    "lastname": "Flintstone",
+    "address": "123 calle de street",
+    "city": "here",
+    "birthdate": "12/12/12",
+    "email": "here@work.com",
+    "phone": "520-123-4567"
+  };
+
+export function VoterForm() {
+    return (
+        <div>
+            <h2>Voter Form</h2>
+        </div>
+    );
+}
--- src/components/VoterRegsitration.tsx
+++ src/components/VoterRegsitration.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import {VoterForm } from './VoterForm'
+
+
+
+
 export function VoterRegsitration() {
     return (
         <div>
+            
             <h2> Welcome to voter Registration</h2>
+
+            <VoterForm />
+
+            {/* <ViewVoter />  */}
         </div>
     );
 }
