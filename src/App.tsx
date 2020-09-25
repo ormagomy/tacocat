9c8acdf140f74c483682dc1ab8d8f418397f9716
jorme <jared_orme@intuit.com>
2020-09-25 11:13:45 -0600

    Added tacocat mascot image

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {makeStyles} from '@material-ui/core';
+
+import tacocat from './images/tacocat.png';
 
 import {VoterRegsitration} from './components/VoterRegsitration';
 import {Home} from './components/Home';
 import {Election} from './components/Election';
 import {Vote} from './components/Vote';
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
+const useStyles = makeStyles({
+    mascot: {
+        width: 100,
+    },
+});
 
 export default function BasicExample() {
+    const classes = useStyles();
+
     return (
         <Router>
             <div>
@@ -34,15 +36,10 @@ export default function BasicExample() {
                     </li>
                 </ul>
 
+                <img className={classes.mascot} src={tacocat} alt="tacocat" />
+
                 <hr />
 
-                {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
                 <Switch>
                     <Route exact path="/">
                         <Home />
